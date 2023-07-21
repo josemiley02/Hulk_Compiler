1f2c63c393504a59b950f4e237370bf64bef8064
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2023-07-21 02:08:54 -0400

    Example

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            string test = "if(a<=(10 + 3) - 5)";
+            System.Console.WriteLine(test);
+            List<Token> tokens = Tokenizer.GetTokens(test);
+            foreach (var item in tokens)
+            {
+                System.Console.WriteLine(item.Types + "," + item.Value);
+            }
         }
     }
 }
